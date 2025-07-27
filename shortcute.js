b58772a573d1deafa5674ca38b6bf857a726061e
deluwarhoisn <babuhossen301@gmail.com>
2025-07-28 00:48:46 +0600

    added refresh

--- shortcute.js
+++ shortcute.js
@@ -1,8 +1,19 @@
-const isLeader = true;
-if (isLeader){
-    console.log('leader ache! chair charo');
-}
+// const isLeader = true;
+// if (isLeader){
+//     console.log('leader ache! chair charo');
+// }
 
+// else{
+//     console.log('vitory dukty devina');
+// }
+
+
+const isPassed = true;
+//  if(ispassed === false)
+if(!ispassed){
+    console.log('toky biya diya dimu');
+
+}
 else{
-    console.log('vitory dukty devina');
+    console.log('tomy amader noyon ar mony')
 }
