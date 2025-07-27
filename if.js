5599be3381d4c19733176b57693b29b1cf81f743
deluwarhoisn <babuhossen301@gmail.com>
2025-07-27 21:38:35 +0600

    added if and eils and true or fols

---
+++ if.js
@@ -0,0 +1,19 @@
+// if STRUCTURE
+//  if (condition){
+// fjkdf}
+
+if (3 < 10){
+    console.log('i have a smaller valo')
+}
+
+var biryaniprice = 250;
+if (biryaniprice < 300){
+    console.log('i will buy biryani')
+}
+var weight = 20;
+if (weight <= 20){
+    console.log('i will buy a weight')
+}
+else {
+    console.log('i will not buy a riksha')
+}
