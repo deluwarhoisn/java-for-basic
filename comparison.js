8e350f325b292f121ccc49355afe3e7b9f0e7614
deluwarhoisn <babuhossen301@gmail.com>
2025-07-27 21:13:49 +0600

    addeed if and eles

---
+++ comparison.js
@@ -0,0 +1,20 @@
+
+// console.log(5 > 10);
+// console.log(5 < 10);
+// console.log(5 == 10);
+// console.log(10 == 10);
+// console.log(10 == 10);
+// console.log(10 >= 10);
+// console.log(5 <= 10);
+// console.log(5 >= 10);
+// console.log(5 <= 10);
+console.log(10 == 10);
+
+
+// // ------------------------
+// var peyaraPrice =40;
+// var appleprice = 250;
+
+// console.log(peyaraPrice > appleprice)
+console.log(10 == '10')
+console.log(10 === '10')
