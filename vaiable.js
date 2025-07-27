74b0a81c802d441226020c2b2513ae1f8a334fdf
deluwarhoisn <babuhossen301@gmail.com>
2025-07-27 20:37:46 +0600

    added javascript basic

---
+++ vaiable.js
@@ -0,0 +1,34 @@
+/**
+ * variable ==> container
+ */
+
+// number
+var sunglassPrice = 80;
+// string
+var subject = "EEE";
+
+// boolaen 
+var passed = true;
+
+/**
+ * NAMING CONVENTION
+ * 1. SINGAL WORD
+ * 2. no quote
+ * 3. no gap or dash
+ * 4. can not start with number
+ * 5. can not be a keyword
+ * 6. prefer camelcase
+ * 
+ * 
+ */
+
+var LastExameResult = 89;
+
+/**
+ * operation
+ * 1. +, -, *, /, %
+ * 2. +=, -=, *=, /=, %=
+ * 3. isNaN
+ * 4. null
+ * 5.number
+ */
