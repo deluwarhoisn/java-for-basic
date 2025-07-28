232b6bc479b3feb2341afec6824f5a76a9e2e2f7
deluwarhoisn <babuhossen301@gmail.com>
2025-07-29 00:41:32 +0600

    added if bangla and einglish

--- if.js
+++ if.js
@@ -27,3 +27,25 @@ var briyaniPrice = 250;
 if (briyaniPrice < 300){
     console.log('mama give me biriyani')
 }
+
+
+/**
+ * if (condition){
+ * উপরের শর্তটি সত্য হলে কোডটি কার্যকর করুন
+ *   execute code if condition above is true
+ * }
+ * 
+ * else{
+ * উপরের শর্তটি মিথ্যা হলে কোডটি কার্যকর করুন
+ *  execute code if condition above is false}
+ */
+
+
+var weight = 20;
+
+if (weight <= 20){
+    console.log('I will carry by my self আমি নিজেই বহন করব।')
+}
+else {
+    console.log(' i will reny a rickshaw আমি একটা রিকশা ভাড়া করবো।')
+}
